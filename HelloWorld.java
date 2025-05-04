8d1a7a76a9e1c48c1f887af259054c7c0c27316d
Sebastian Mejia <Sebastianmejia200625@gmail.com>
2025-05-03 23:50:10 -0400

    Add HelloWorld.java with initial classes and age input prompt

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Heloo, GCDISS!234!");
+        
+    }
+}
+class Variablepractivce2 {
+    public static void main(String[] args) {
+        int nnmber = 42;
+        double pi = 3.14;
+        char letter = 'g';
+
+        System.out.println("Int: " + nnmber);
+        System.out.println("Double:" + pi);
+        System.out.println("Char: " + letter);
+    }
+}
+
+class AgeCheck {
+    public static void test(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        
+    }
+}
